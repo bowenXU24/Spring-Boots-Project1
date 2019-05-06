3224baba4c31427ed19ae14ed479b6f45577103f
Bowen <bowenxu@me.com>
2019-05-06 11:51:59 -0400

    test devtools dependencies for automic deployment

--- src/main/java/com/springboot/demo/myApp/rest/FunRestController.java
+++ src/main/java/com/springboot/demo/myApp/rest/FunRestController.java
@@ -15,4 +15,15 @@ public class FunRestController {
     public String sayHello(){
         return "Hi the time is" + LocalDateTime.now();
     }
+
+    //expose new edn point for workout
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "push ups";
+    }
+    //test end point
+    @GetMapping("/dota")
+    public String getDota(){
+        return "2 ez";
+    }
 }
