8b50a6c3f568400a56730a9249ff4cc3ba36df1c
Bowen <bowenxu@me.com>
2019-05-06 15:30:22 -0400

    add some testing property

--- src/main/java/com/springboot/demo/myApp/rest/FunRestController.java
+++ src/main/java/com/springboot/demo/myApp/rest/FunRestController.java
@@ -2,6 +2,7 @@ package com.springboot.demo.myApp.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class FunRestController {
+
+    //inject properties
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: " + coachName + " from " + teamName;
+    }
 
     @GetMapping("/")
     public String sayHello(){
